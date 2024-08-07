1238058a56e311455c7b80d1330d69e7a8e7a56b
Muhammad faraz khan <47053490+Muhammadfarazkhan123@users.noreply.github.com>
2024-08-08 03:15:11 +0500

    changes

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
+//chapter: variables for number:
+
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box:
 var age = " 25 Years Old";
 alert ("I am"+ " "+age);
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”:
 var visited_time = "10 times";
 document.write("You have visited this site"+" "+visited_time);
 var birth_year = 1999;
 document.write("<br><br> My birth year is"+" "+birth_year);
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
 var data_type ="<br> <br>Data type of my variable is number.";
 document.write(data_type);
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”:
+
 var Visitor_name="<br><br><b>John Deo</b>";
 document.write(Visitor_name);
 var quantity="<b> 5 T-Shirts</b>";
